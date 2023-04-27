c940ad8b5a16e71db0ec00000265a59fa603517b
alifertah <alfertah@student.1337.ma>
2023-04-27 12:51:49 +0100

    ✅

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# BLS-scipt
+
+# Appointment Checker Script
+
+This script can be used on the website "[https://www.blsspainmorocco.net/mar/blsappointment/manageappointment](https://www.blsspainmorocco.net/mar/blsappointment/manageappointment)" in conjunction with the Tampermonkey browser extension. Its purpose is to help you find available appointment slots by automatically checking for open spots in different cities and alerting you when a green spot is found.
+
+## Usage
+
+1.  Install the Tampermonkey extension in your browser.
+2.  Install the Appointment Checker Script by copying and pasting the code into a new script in Tampermonkey.
+3.  Navigate to the "[https://www.blsspainmorocco.net/mar/home/index](https://www.blsspainmorocco.net/mar/home/index)" website.
+4.  The script will automatically start running, cycling through different cities and checking for green appointment slots.
+5.  When a green spot is found, you will be notified so that you can quickly secure the appointment.
+
+## Disclaimer
+- This script is intended to be used for educational and informational purposes only. The author of this script is not responsible for any illegal use of the script or any consequences that may result from such use. Please use this script at your own risk and make sure to comply with all applicable laws and regulations.
+- Please note that this script is provided as-is and may not always work as expected. Use at your own risk.
---
+++ checker.js
@@ -0,0 +1,76 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       alifertah
+// @match        https://www.blsspainmorocco.net/mar/blsappointment/manageappointment
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=blsspainmorocco.net
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    var audio = document.createElement('audio');
+    audio.src = '//upload.wikimedia.org/wikipedia/commons/4/40/Toreador_song_cleaned.ogg';
+    document.body.appendChild(audio);
+    console.log("audio")
+
+
+    var newPopup = "https://youtu.be/kcT-i9xzC-8";
+    var myInterval = setInterval(function() {
+        //clicking the first popup's buttons
+        document.querySelector('button.btn.btn-success[onclick="return OnUserConsentAccept();"]').click();
+        var childInterval = setInterval(function() {
+            document.querySelector('button.btn-close').click();
+            clearInterval(childInterval)
+        }, 1000)
+        // filling the city's input
+        function handleClick(item) {
+            // Do something before clicking the item
+            console.log("Clicking item", item);
+
+            // Run the additional code
+            document.querySelector('.k-icon.k-i-calendar').click();
+
+            var linkElements = document.querySelectorAll('a.k-link');
+            for (var i = 0; i < linkElements.length; i++) {
+                var test = window.getComputedStyle(linkElements[i])
+                if (test.backgroundColor !== "rgb(255, 191, 0)" && test.backgroundColor !== "rgb(214, 41, 62)" && test.backgroundColor !== "rgb(116, 117, 121)" && test.backgroundColor !== "rgba(0, 0, 0, 0)") {
+                } else {
+                    clearInterval(interval);
+                    clearInterval(myInterval);
+                    window.open(newPopup, "_blank");
+                    newPopup = "";
+                    audio.play();
+                    alert("there is a green spot in " + $(".k-list.k-reset#LocationId_listbox li").eq(i).text()  + " calendar");
+                    return;
+                    console.log("not green")
+                }
+                const okButton = document.querySelector('#commonModal .btn-close');
+                okButton.click();
+            }
+            // Click the item
+            const okButton = document.querySelector('.btn-success');
+            okButton.click();
+            $(".k-list.k-reset#LocationId_listbox li").eq(item).click();
+        }
+
+        var i = 0;
+        var interval = setInterval(function() {
+            if (i < 6) {
+                handleClick(i);
+                i++;
+            } else {
+                location.reload()
+                clearInterval(interval);
+            }
+        }, 1000); // delay of 1000 milliseconds (1 second)
+
+        if ($(".k-list.k-reset#LocationId_listbox li").length > 0) {
+            console.log("ana hna")
+            clearInterval(myInterval);
+        }
+    }, 5000);
+
+})();
